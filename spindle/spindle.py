b42f3c5af69f2701e2dcaa38f53ae0683633a82b
Michał Poręba <michalporeba@gmail.com>
2021-10-07 22:05:00 +0100

    making 'privates' a little bit more private with name mangling

--- spindle/spindle.py
+++ spindle/spindle.py
@@ -6,26 +6,26 @@ import types
 
 class Spindle():
     def __init__(self):
-        self._threads = {}
+        self.__threads = {}
         
     def add_model(self, source: str, model: Union[types.SimpleNamespace, dict]):
         if model: 
-            for k, v in self._model_to_dict(model).items():
+            for k, v in self.__model_to_dict(model).items():
                 self.add_message(Message(source, k, v))
                 
     def add_message(self, *messages: Message):
         for message in messages:
-            if not message.property in self._threads:
-                self._threads[message.property] = Thread()
-            self._threads[message.property].add(message)
+            if not message.property in self.__threads:
+                self.__threads[message.property] = Thread()
+            self.__threads[message.property].add(message)
         
     def values(self) -> dict:
-        return {k: v.value() for k, v in self._threads.items()}
+        return {k: v.value() for k, v in self.__threads.items()}
 
     def conflicts(self) -> list:
-        return {k: v for k, v in self._threads.items() if isinstance(v.value(), Conflict)}.keys()
+        return {k: v for k, v in self.__threads.items() if isinstance(v.value(), Conflict)}.keys()
 
-    def _model_to_dict(self, model: Union[types.SimpleNamespace, dict]) -> dict:
+    def __model_to_dict(self, model: Union[types.SimpleNamespace, dict]) -> dict:
         if isinstance(model, dict):
             return {k: v for k, v in model.items() if not k.startswith('_')}
         else: 
--- spindle/thread.py
+++ spindle/thread.py
@@ -4,7 +4,7 @@ from .message import Message
 class Thread():
     def __init__(self):
         self.messages = []
-        self._current = None
+        self.__current = None
         
     def add(self, message: Message):
         if len([m for m in self.messages if m.signature == message.signature]):
@@ -12,15 +12,15 @@ class Thread():
 
         self.messages.append(message)
         if len(self.messages) == 1:
-            self._current = message.value    
+            self.__current = message.value    
         else:
-            self._current = Conflict(self.messages)        
+            self.__current = Conflict(self.messages)        
         
     def value(self):
-        return self._current
+        return self.__current
 
     def flat_value(self):
-        if isinstance(self._current, Conflict):
+        if isinstance(self.__current, Conflict):
             return None
-        return self._current
+        return self.__current
     
